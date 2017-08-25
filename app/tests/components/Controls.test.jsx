be6395fd474033e19851e0258ae404894a21a275
IonescuA <alina.ionescu@haufe.-lexware.com>
2017-08-25 16:01:49 +0300

    added pause button test

--- app/tests/components/Controls.test.jsx
+++ app/tests/components/Controls.test.jsx
@@ -20,12 +20,12 @@ describe ('Controls', () => {
             expect($pauseButton.length).toBe(1);
         });
 
-        it('should render start when started', () => {
-            var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started"/>);
+        it('should render start when paused', () => {
+            var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused"/>);
             var $el = $(ReactDOM.findDOMNode(controls));
-            var $pauseButton = $el.find('button:contains(Pause)');
+            var $startButton = $el.find('button:contains(Start)');
 
-            expect($pauseButton.length).toBe(1);
+            expect($startButton.length).toBe(1);
         });
     });
 });
