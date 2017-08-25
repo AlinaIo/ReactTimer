beb420affb57b641e9440c514e927475d79a6a24
IonescuA <alina.ionescu@haufe.-lexware.com>
2017-08-25 15:56:50 +0300

    added Controls component and tests and motion-ui fader and orbit

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -1,9 +1,25 @@
 var React = require('react');
 
-var Controls = () => {
-    return (
-        <h1>Controls component</h1>
-    )
-};
+var Controls = React.createClass({
+    propTypes: {
+        countdownStatus: React.PropTypes.string.isRequired
+    },
+    render: function () {
+        var {countdownStatus} = this.props;
+        var renderStartStopButton = () => {
+            if(countdownStatus === 'started') {
+                return <button className="button secondary">Pause</button>
+            } else if (countdownStatus === 'paused') {
+                return <button className="button Primary">Start</button>
+            }
+        }
+        return (
+            <div>
+                {renderStartStopButton()}
+                return <button className="button alert hollow">Clear</button>
+            </div>
+        )
+    }
+});
 
 module.exports = Controls;
